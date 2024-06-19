b5e7f4e1a2347629d1b65c9d0effc5e160fe9662
Sergej200312 <sergej-mixajlov-2003@bk.ru>
2024-06-19 23:55:04 +0500

    создание модели для user

---
+++ models/user.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    username: { 
+        type: String, 
+        required: true, 
+        minlength: 2,
+        maxlength: 20,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 4,
+        maxlength: 12,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ['USER', 'ADMIN'],
+        default: 'USER'
+    }
+});
+
+module.exports = mongoose.model("User", UserSchema);
+
+
