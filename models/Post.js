025993042d46ede72227274b5a85935720b5ab45
Sergej200312 <sergej-mixajlov-2003@bk.ru>
2024-06-20 00:00:55 +0500

    создал модель для постов

---
+++ models/Post.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    },
+    title: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 100
+    },
+    content: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 1000
+    },
+    date: {
+        type: Date,
+        required: true
+    }
+})
+
+module.exports = mongoose.model("Post", PostSchema);
